831b9135f76fd88c004097d6dae149fecb078c77
Brandon Mizuno <brandon.mizuno@gmail.com>
2017-07-14 23:14:42 -0700

    first commit

---
+++ server/models/TaskModel.js
@@ -0,0 +1,13 @@
+// v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
+const myURI = '';
+
+// UNCOMMENT THE LINE BELOW IF USING MONGO
+// const URI = process.env.MONGO_URI || myURI;
+
+// UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
+// const URI = process.env.PG_URI || myURI;
+
+
+
+
+module.exports = null; // <-- export your model
